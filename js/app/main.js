32371f0924640adb5ba1e43a4f7e03a04adbe100
Chris Snyder <bumishness@gmail.com>
2014-04-28 15:05:25 -0500

    bugfix

--- js/app/main.js
+++ js/app/main.js
@@ -2,10 +2,9 @@ function populateInstagram() {
   var feed = new Instafeed({
           clientId: '4d92c60c642241a28c4c95c8d140ba41',
           get: 'tagged',
-          tagged: 343239464,
           tagName: 'spacecation',
           limit: 8, 
-          template: '<li><a class="instagram-image" href="{{link}}"><img class="instagram-image" src="http://{{image}}"/></a></li>'
+          template: '<li><a class="instagram-image" href="{{link}}"><img class="instagram-image" src="{{image}}"/></a></li>'
         });
   feed.run();
 }
