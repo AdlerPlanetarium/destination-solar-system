73adb5447243b36a8b77a9dd4a00186d5ce11bca
Steve <steve@zooniverse.org>
2014-06-10 16:16:15 -0500

    Obtain AWS credentials from environment variable

--- build.rb
+++ build.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/env ruby
 require 'aws-sdk'
-AWS.config access_key_id: 'AKIAIQZ2TDA5U6GEFWYA', secret_access_key: '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'
-
+AWS.config access_key_id: ENV['ADLER_S3_ACCESS_ID'], secret_access_key: ENV['ADLER_S3_SECRET_KEY']
 s3 = AWS::S3.new
 staging_bucket = 'test-dss.adlerplanetarium.org'
 prod_bucket = 'destinationsolarsystem.adlerplanetarium.org'
